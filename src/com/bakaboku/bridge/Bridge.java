be26ad0e7e36e7a551867a2f4cd49d8f1b41e403
mato <uedeck@gmail.com>
2014-11-19 15:09:17 +0800

    init

---
+++ src/com/bakaboku/bridge/Bridge.java
@@ -0,0 +1,30 @@
+package com.bakaboku.bridge;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+
+public class Bridge extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		String extra = getIntent().getStringExtra(Intent.EXTRA_TEXT);
+		String url = "";
+		Pattern pattern = Pattern.compile("htt(p|ps)[\\w\\d\\:\\/\\.]{3,}");
+		Matcher matcher = pattern.matcher(extra);
+		if (matcher.find()) {
+			url = matcher.group();
+		}
+		if (TextUtils.isEmpty(url))
+			finish();
+		Log.i("BrowserBridge", url);
+		Intent intent = new Intent(Intent.ACTION_VIEW);
+		intent.setData(Uri.parse(url));
+		startActivity(intent);
+	}
+}
